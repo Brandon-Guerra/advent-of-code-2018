9b7882950b1772d5c554a0a1c744d81e36463a4c
Brandon <brandon.guerra@pwc.com>
2018-12-13 00:45:44 -0600

    added part 2 solution

--- src/01/main.c
+++ src/01/main.c
@@ -1,16 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int main() {
 	FILE *input;
 	char data[100];
 	int total = 0;
-	input = fopen("frequency_changes.txt", "r");
-	while (fgets(data, 100, input) != NULL) {
-		total += atoi(data);
+	int repeat = 0;
+	int firstIteration = 0;
+	int positives[1000000] = { 0 };
+	int negatives[1000000] = { 0 };
+	while (repeat == 0) {
+		input = fopen("frequency_changes.txt", "r");
+		while (fgets(data, 100, input) != NULL) {
+			total += atoi(data);
+			if (total < 0) {
+				if (negatives[abs(total)] == 1 && repeat == 0) {
+					printf("Part 2: %d\n", total);
+					repeat = 1;
+				}
+				negatives[abs(total)] = 1;
+			}
+			else {
+				if (positives[total] == 1 && repeat == 0) {
+					printf("Part 2: %d\n", total);
+					repeat = 1;	
+				}
+				positives[total] = 1;
+			}
+		}
+		if (firstIteration == 0) {
+			printf("Part 1: %d\n", total);
+			firstIteration ++;
+		}
 	}
 	fclose(input);
-	printf("Part 1: %d\n", total);
-	return total;
+
+	return 0;
 }
